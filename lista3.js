d72d70f6c9a83c9b9ee3afe74d299a5de087ab3a
Bozano <joao.bazan@escola.pr.gov.br>
2026-03-05 14:11:36 +0000

    lista

---
+++ lista3.js
@@ -0,0 +1,30 @@
+//Criar um pograma para verificar se o número 12 é divisivel por 2 e por 3
+let numero = 12;
+if (numero % 2 === 0 && numero % 3 === 0) {
+    console.log(numero + " é divisível por 2 e por 3.");
+} else {
+    console.log(numero + " não é divisível por 2 e por 3.");
+}
+//Criar um programa para mostrar um desconto de 12% para produtos acima de R$800
+let preco = 800;
+if (preco >= 800) {
+    let desconto = preco * 0.88;    
+    console.log(`O preço com desconto é ${desconto}`)
+} else {
+    console.log('não tem desconto')
+}
+//Criar um programa que mostre os números de 1 a 10 usando laço de repetição
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+//Criar um programa para mostrar apenas os números pares entre 10 e 1
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+    }
+//Criar um programa para mostrar apenas os números impares entre 1 e 20
+//Criar um programa para mostrar a tabuada do 5
+//Criar um programa para mostrar os números de 1 a 7 usando while
+//Criar um programa para mostrar a tabuada do 3 usando while
+//Criar um programa para mostrar o fatorial de 5
+//Criar um programa para saber se o número 7 é primário
+//Criar um programa para calcular a potência de 2³
